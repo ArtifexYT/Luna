3200b3b94938036f53cb41f642988398bdab9c67
Damian <46952101+ArtifexYT@users.noreply.github.com>
2019-01-23 18:26:18 -0500

    Match the other files.

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-module.exports = async client => {
+module.exports = async (client) => {
   client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   client.user.setActivity(`${client.users.size} Users | V${client.instance.version}`, {
