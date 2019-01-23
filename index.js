560b84e8deec133e5133db849576bb3e99f987f8
Damian <46952101+ArtifexYT@users.noreply.github.com>
2019-01-23 11:06:32 -0500

    Removed LevelDB

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const Discord = require("discord.js");
 const { promisify } = require("util");
 const readdir = promisify(require("fs").readdir);
 const Enmap = require("enmap");
-const EnmapLevel = require("enmap-level");
 
 const client = new Discord.Client({
     disableEveryone: true,
