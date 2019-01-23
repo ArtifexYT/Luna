703cc95ff7511ccd9f8bdb3216f7cc3d86fb914e
Damian <46952101+ArtifexYT@users.noreply.github.com>
2019-01-23 17:55:57 -0500

    Finish main file.

--- index.js
+++ index.js
@@ -38,5 +38,30 @@ client.settings = new Enmap({
 });
 
 const init = async () => {
-    
+  
+  const cmdFiles = await readdir("./commands/");
+  client.logger.log(`Loading a total of ${cmdFiles.length} commands.`);
+  cmdFiles.forEach(f => {
+    if (!f.endsWith(".js")) return;
+    const response = client.loadCommand(f);
+    if (!response) console.log(response);
+  });
+  
+  const evtFiles = await readdir("./event");
+  client.logger.log(`Loading a total of ${evtFiles.length} events.`);
+  evtFiles.forEach(file => {
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
+  });
+  
+  client.levelCache = {};
+  for (let i = 0; i < client.config.permLevels.length; i++) {
+    const thisLevel = client.config.permLevels[i];
+    client.levelCache[thisLevel.name] = thisLevel.level;
+  }
+  
+  client.login(client.config.token);
 };
+
+init();
